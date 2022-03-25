1f83c3d33cce9f5065db741e6a217695f606e0d4
ahadmeer <abdulahadmeer97@gmail.com>
2022-03-25 14:50:18 -0700

    javascript Assginment

---
+++ chap1.js
@@ -0,0 +1,168 @@
+// *================================= FRIST CHAP ===============================*
+
+// alert ("Well Come To My Websit");
+
+// alert ("Error! please enter a valid password.");
+    
+// alert ("Wellcome to js land...\n Happy Coding!") ; 
+
+// alert ("Wellcome to js land...");
+ 
+// alert("Happy Coding! \n prevent this page from creating additional dialogs.");
+  
+// alert("Hello... I can run js trough my web browsers consols");
+
+// *======================== FIRST CHAP END ==================================*
+
+
+
+// *=================== SECOND CHAP ==================================*
+
+
+// var userName = "abdul ahad"
+// console.log(userName);
+
+// var myName = "abdul ahad"
+// document.write(myName)
+
+
+// var  titledMessage = "Hello World";
+// alert (titledMessage)
+
+// var StudentName = "ahad";
+// alert(StudentName);
+
+// var age = 15;
+// alert(15);
+
+// var coures =  "Certified Mobile Application Develoment";
+// alert(coures);
+
+
+// var x = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert (x);
+ 
+// var Email = "abdulahadmeer97@gmail.com";
+// alert (Email);
+
+// var Book = "I am trying to learn from the book A smarter \n Way to learn JavaScript";
+// alert(Book);
+
+// document.write("Yah! I can Write HTML content through JavaScript")
+
+
+// var a =  "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert (a);
+// document.write(a)
+
+// *============================== SECOND CHAP END ================================*
+
+
+// *============================================= THIRD CHAP =======================================================*
+
+
+// var age = 15;
+// alert ("iam "+ age + " years old")
+
+
+// var visitToWeb=1;
+// if(window.localStorage.getItem('visits'))
+// {
+//     visitToWeb=window.localStorage.getItem(visitToWeb);
+//     visitToWeb++;
+// }
+// else
+// {
+//     window.localStorage.setItem('visits',visitToWeb);
+// }
+// alert("You have visited "+visitToWeb+" times");
+
+
+// var birthYear =  1997 
+// document.write("My birth year is" + birthYear +  " <br> Data type of my ddeclared variables is number");
+
+// var visitorName = "Ahad"
+// var Quantity = 5;
+// var product = "T-shirts(s)"
+// document.write( visitorName + " Ordered " + Quantity + "  " + product + " on XYZ Clothing store ");
+
+// *===================================== THIRD CHAP END ===========================================================*
+
+
+// *========================================= FOURTH CHAP ===========================================================*
+// var a,b,c
+// document.write ("<h1> Rules For naming JS variables </h1>");
+
+// document.write(" Variable names can only contain, numbers, $ and _ .For example : $my _ 1st variable ,<br>");
+
+// document.write(" Variable must begin with a letter, $ or _ .For example : $name, _name or name <br>");
+
+// document.write( "Variable names are case sensitive <br>")
+
+// document.write("Variabe names should not be JS keyword")
+
+// *============================================ FOURTH CHAP END ===============================================================*
+   
+// var a = 3
+// var b = 5
+// var result = a + b;
+
+// document.write("sum of " + a + " and " + b + " is " + result);
+
+
+// var a = 10
+// var b = 5
+// var result = a - b;
+
+// document.write(" sub of " + a + " and " + b + " is " + result + "<br>");
+
+
+
+// var a = 10
+// var b = 10
+// var result = a * b;
+
+// document.write(" multi of " + a + " and " + b + " is " + result + "<br>");
+
+
+
+// var a = 12
+// var b = 30
+// var result = a / b;
+
+// document.write("divide of " + a + " and " + b + " is " + result + "<br>");
+
+
+// var x = 2;
+// // document.write("Variable after declration is ",x);
+// // document.write(" <br> intial value: ",x);
+// // x++;
+// // document.write("<br> Variable after increment is ",x);
+// // x=x+7;
+// // document.write("<br> Variable after addition is ",x);
+// // x--;
+// // document.write("<br> Variable after decremet is ",x);
+
+// // x=x%3;
+
+// // document.write("<br>Reminder is",x);
+
+
+
+// var ticket=600;
+
+// document.write("Buying 5 tickets price is ", ticket*5);
+
+
+var temperature=40;
+var f = (temperature*9/5)+32;
+
+document.write("Temperature in Farenhiet is ",f);
+
+var c=(f-32) * 5/9; 
+
+ document.write("<br>Temperature in celsuis is ",c);
+
+
+
+
