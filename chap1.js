f2970acf447a42e065e4355dc22ab075fe4ffe5d
ahadmeer <abdulahadmeer97@gmail.com>
2022-03-29 14:51:40 -0700

    assignments

--- chap1.js
+++ chap1.js
@@ -102,7 +102,12 @@
 // document.write("Variabe names should not be JS keyword")
 
 // *============================================ FOURTH CHAP END ===============================================================*
-   
+
+
+
+
+// *============================================ FIFTH CHAP ================================================================*
+
 // var a = 3
 // var b = 5
 // var result = a + b;
@@ -154,14 +159,157 @@
 // document.write("Buying 5 tickets price is ", ticket*5);
 
 
-var temperature=40;
-var f = (temperature*9/5)+32;
+// var temperature=40;
+// var f = (temperature*9/5)+32;
 
-document.write("Temperature in Farenhiet is ",f);
+// document.write("Temperature in Farenhiet is ",f);
 
-var c=(f-32) * 5/9; 
+// var c=(f-32) * 5/9; 
 
- document.write("<br>Temperature in celsuis is ",c);
+//  document.write("<br>Temperature in celsuis is ",c);
+
+
+// var  prizeofitem1 = 650;
+// var  prizeofitem2 = 100;
+// var  orderQunatity1 = 3;
+// var  orderQuantity2 = 7;
+// var  shippingCharges = 100;
+
+// document.write(" prize of item 1 is " + prizeofitem1 + "<br>" );
+
+// document.write( "Ordered quantity of item 1 is " + orderQunatity1 + "<br>");
+
+// document.write("prize of item 2 is " + prizeofitem2 + "<br>");
+
+// document.write("order Quantity 2 is " + orderQuantity2 + "<br>");
+
+// document.write("shipping Charges " + shippingCharges + "<br>")
+
+// var result = prizeofitem1 * 3 + prizeofitem2 *7 + shippingCharges ;
+
+// document.write("Total cost of your order is " + result) 
+
+// var totalMark = 980;
+// var markOtained = 804;
+
+// var result = markOtained/totalMark *100;
+// document.write(" precentage " + result)
+
+
+// var usDollars = 10
+// var saudiRiyals = 25
+
+// var result = usDollars * 104.80 + saudiRiyals * 28;
+// document.write ("Total currency in PKR " + result) ;
+
+
+// var someNumber=9;
+// var result = someNumber  + 5 * 10 ;
+
+// document.write("result :",result/2);
+
+
+// var currentYear = 2022;
+// var birthYear = 1997;
+// var TotalAge = currentYear - birthYear;
+// document.write("your age " + TotalAge);
+
+
+// var r=20;
+// var circumferenceOfArea = 2*3.142*r;
+// document.write("Circumfrence of area is :",circumferenceOfArea +"<br>");
+// var areaOfacicle = 3.142* r *2;
+// document.write("area Of acicle " + areaOfacicle)
+
+
+// *======================================= FIFTH CHAP END ===================================================*
+
+
+// *=========================================== SIXTH TO NINTH CHAP =================================================*
+
+
+// var a = 10
+// document.write("Result: <br> The value of a is: "  + a );
+
+// ++a;
+// document.write(" <br> The value of ++a is: " + a +  " <br> Now the value of a is:" + a + "<br>");
+
+// document.write("The value of a++ is: " + a + "<br>");
+
+
+// ++a;
+// document.write(" Now the value of a is: " + a);
+
+// --a;
+// document.write(" <br> The value of --a is: " + a);
+
+// document.write(" <br> Now the value of a is: " + a);
+
+// document.write("<br> The value of a-- is: " + a );
+// a--;
+// document.write(" <br> Now the value of a is: " + a );
+
+
+
+// var a = 2;
+// var b = 1;
+// var result =  --a - --b + ++b + b--;
+// document.write(" <br> --a is ",--a);
+// document.write(" <br> --b is ",--b);
+// document.write(" <br> ++b is ",++b);
+// document.write(" <br> b-- is ",b--);
+// document.write(" <br> Final result is ",result)
+
+// var userName =  prompt ("Enter user name")
+// document.write("hello" + userName)
+
+
+
+// var num = prompt("enter number whom table you want to print");
+
+// document.write(num*1  +"<br>");
+// document.write(num*2  +"<br>");
+// document.write(num*3  +"<br>");
+// document.write(num*4  +"<br>");
+// document.write(num*5  +"<br>");
+// document.write(num*6  +"<br>");
+// document.write(num*7  +"<br>");
+// document.write(num*8  +"<br>");
+// document.write(num*9  +"<br>");
+// document.write(num*10  +"<br>");
+
+// var english =+prompt ("Enter English marks");
+// var urdu   =+prompt ("Enter urdu marks");
+// var math   =+prompt ("Enter math marks");
+// var totalMarks = 100;
+// var precentageEnglish = english/totalMarks*100
+// var precentageMath = urdu/totalMarks*100
+// var precentageUrdu = math/totalMarks*100
+// document.write("<h1> Subject Total Marks Obtained Marks Precentage </h1>" )
+// document.write("english   " + totalMarks + "  Obtained marks  " + english + " <br> " + " urdu " + totalMarks + " Obtained marks " + urdu + "  <br> "  + " math "+ totalMarks + "   " +" Obtained marks "   + math +" "+  precentageEnglish  +" "+ precentageMath +" "+ precentageUrdu )
+// var add = english + urdu + math
+// document.write("Total Numbers are :"+ add)
+// var allpaparmarks = totalMarks+totalMarks+totalMarks
+// document.write(allpaparmarks )
+
+
+var city = prompt("enter the city name")
+if(city=="karachi")
+{
+document.write("welcome to karachi")
+}
+else
+{
+    document.write("unKown");
+    
+    
+}
+
+
+
+
+
+ 
 
 
 
